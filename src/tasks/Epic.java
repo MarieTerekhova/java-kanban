d2e9696816d937cf17f181a7c973c58b85ff3012
Marie Terekhova <eroshenko.m@gmail.com>
2025-05-21 17:19:04 +0700

    Ирек, привет! Сначала поменяла в методе getSubtasksList() возвращаемый параметр на new ArrayList<>(), как ты рекомендовал, но, к сожалению, у меня перестала работать проверка в методе вычесления статуса Эпика updateEpicStatus(int epicId). Поэтому я пока вернула значение, которое у меня было. Мне не хватает понимания, как переделать метод проверки с возвращаемым new ArrayList<>() в getSubtasksList().
    Очень надеюсь, что это не критичная ошибка.

--- src/tasks/Epic.java
+++ src/tasks/Epic.java
@@ -1,4 +1,5 @@
 package tasks;
+
 import java.util.ArrayList;
 
 public class Epic extends Task {
@@ -11,16 +12,29 @@ public class Epic extends Task {
     }
 
     public ArrayList<Integer> getSubtasksList() {
+        //return new ArrayList<>(); -- перестал работать метод проверки статуса Эпика
         return subtasksList;
     }
 
     public void addSubtask(int subtaskId) {
-        subtasksList.add(subtaskId);
+        if (!subtasksList.contains(subtaskId)) {
+            subtasksList.add(subtaskId);
+            {
+            }
+        }
+    }
+
+    public void removeSubtask(int subtaskId) {
+        subtasksList.remove((Integer) subtaskId);
+    }
+
+    public void clearSubtasks() {
+        subtasksList.clear();
     }
 
     @Override
     public String toString() {
-        return "\nEpic: {" +
+        return "Epic: {" +
                 "id: " + getId() + ", название: " + "'" + getNameTask() + "'"
                 + ", описание: " + "'" + getDescriptionTask() + "'"
                 + ", статус: " + "'" + getTaskStatus() + "'"
